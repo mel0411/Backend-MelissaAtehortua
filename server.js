aa80c750fe9e582fb1f3e541e0928d211adfd594
mel0411 <melissaatehortuap>
2025-11-19 19:40:25 -0500

    Se agregaron nuevas funciones a la carpeta de models, Reseñas y se cambio el nombre juegos por Juegos.

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+//improtacion de bloques
 require("dotenv").config();
 
-const express = require("express");
-const mongoose = require("mongoose");
-
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+app.use(express.json());
+
+//conexion con la db
+mongoose.connect(MONGODB_URL)
+    .then(()=> {
+        console.log('Conexion exitosa a MondoDB Atlas');
+    })
+    .catch(err => {
+        console.log('Error de conexion', err.message);
+    })
+
+//rutas
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en htpp://localhost:${PORT}`)
+})
